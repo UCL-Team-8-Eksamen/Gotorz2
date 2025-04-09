66240e377e37843a0cff9927fe6b2ff20677c9f1
Line Kargaard <lika62310@edu.ucl.dk>
2025-04-09 10:23:08 +0200

    Tilføjet v. Husein

---
+++ Testtest/Testtest.Client/Services/TravelApiService.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Json;
+using Testtest.Client.Models;
+
+namespace Testtest.Client.Services
+{
+    public class TravelApiService
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public TravelApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        // Metode til at hente data fra API'et
+        public async Task<List<Travel>> GetTravelsAsync()
+        {
+            try
+            {
+                var travels = await _httpClient.GetFromJsonAsync<List<Travel>>("api/travels");
+                return travels ?? new List<Travel>();
+            }
+            catch (Exception ex)
+            {
+                // Håndter eventuelle fejl, som kan opstå under API kaldet
+                Console.WriteLine($"Error retrieving travels: {ex.Message}");
+                return new List<Travel>();
+            }
+        }
+    }
+}
---
+++ Testtest/Testtest/Program.cs
@@ -0,0 +1,40 @@
+using Testtest.Client.Services;
+using Testtest.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddHttpClient<TravelApiService>();
+
+//Register TravelApiService
+builder.Services.AddScoped<TravelApiService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Testtest.Client._Imports).Assembly);
+
+app.Run();
