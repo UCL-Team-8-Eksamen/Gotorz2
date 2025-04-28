d032de4edad9c960db68f97e066010e8a623212a
Husein Schuta <Hanihusi2x@gmx.com>
2025-04-28 06:00:17 +0200

    yes

--- Gotorz2/Gotorz2.Client/Program.cs
+++ Gotorz2/Gotorz2.Client/Program.cs
@@ -1,12 +1,12 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Gotorz2.Client.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// Configure HttpClient to communicate with the API
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7023/") }); // Replace with your API URL
-
-// Register TravelApiService
-builder.Services.AddScoped<TravelApiService>();
+// Brug HttpClientFactory korrekt!
+builder.Services.AddHttpClient<TravelApiService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7023/");
+});
 
 await builder.Build().RunAsync();
