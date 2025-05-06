46a9737086aa1513a87ad26fb9c49f58718542e9
Husein Schuta <Hanihusi2x@gmx.com>
2025-05-06 14:47:25 +0200

    travelpackage creation 2

--- Gotorz2/Gotorz2.Client/Models/TravelPackage.cs
+++ Gotorz2/Gotorz2.Client/Models/TravelPackage.cs
@@ -3,9 +3,11 @@
 
     public class TravelPackage
     {
+        public string TripTitle { get; set; } = string.Empty;
         public RoundTripFlight Flight { get; set; }
         public AccommodationData Hotel { get; set; }
         public string Description { get; set; }
+        public decimal TripTotalPrice { get; set; }
     }
 
 
