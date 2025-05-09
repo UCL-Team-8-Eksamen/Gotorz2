3a6f0ca768d1bc566ee2c3cc504949e921fc1c04
Line Kargaard <lika62310@edu.ucl.dk>
2025-05-09 10:47:24 +0200

    Sidste fra Husein.

--- Gotorz2/Gotorz2/Program.cs
+++ Gotorz2/Gotorz2/Program.cs
@@ -8,15 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-	.AddInteractiveServerComponents()
-	.AddInteractiveWebAssemblyComponents();
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddHttpClient<TravelApiService>();
 
 
 //Register TravelApiService
 builder.Services.AddScoped<TravelApiService>();
-//Register HotelApiService
+//Register AccommodationApiService
 builder.Services.AddScoped<AccommodationApiService>();
 //Service som holder styr p� valgt fly og hotel
 builder.Services.AddSingleton<TravelPackageState>();
@@ -32,16 +32,16 @@ builder.Services.AddTransient<IUserDatabase, UserDatabase>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
-	options.TokenValidationParameters = new TokenValidationParameters
-	{
-		ValidateAudience = true,
-		ValidAudience = "https://localhost:7180/",
-		ValidateIssuer = true,
-		ValidIssuer = "https://localhost:7180/",
-		ValidateLifetime = true,
-		ValidateIssuerSigningKey = true,
-		IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("524C1F22-6115-4E16-9B6A-3FBF185308F2")) // NOTE: THIS SHOULD BE A SECRET KEY NOT TO BE SHARED; A GUID IS RECOMMENDED, DO NOT REUSE THIS GUID
-	};
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidAudience = "https://localhost:7180/", 
+        ValidateIssuer = true,
+        ValidIssuer = "https://localhost:7180/", 
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("524C1F22-6115-4E16-9B6A-3FBF185308F2")) // NOTE: THIS SHOULD BE A SECRET KEY NOT TO BE SHARED; A GUID IS RECOMMENDED, DO NOT REUSE THIS GUID
+    };
 });
 // Login
 
@@ -50,13 +50,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseWebAssemblyDebugging();
+    app.UseWebAssemblyDebugging();
 }
 else
 {
-	app.UseExceptionHandler("/Error", createScopeForErrors: true);
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -68,9 +68,9 @@ app.UseAntiforgery();
 app.UseAuthentication(); //Login
 
 app.MapRazorComponents<App>()
-	.AddInteractiveServerRenderMode()
-	.AddInteractiveWebAssemblyRenderMode()
-	.AddAdditionalAssemblies(typeof(Gotorz2.Client._Imports).Assembly);
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Gotorz2.Client._Imports).Assembly);
 
 //Login
 app.MapControllers();
