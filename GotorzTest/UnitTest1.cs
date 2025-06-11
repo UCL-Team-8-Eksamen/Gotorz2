f0dd17377cefa48afabf6336b53c381f41dcd3c9
Line Kargaard <lika62310@edu.ucl.dk>
2025-06-11 09:22:58 +0200

    Tilføjet test.

--- GotorzTest/UnitTest1.cs
+++ GotorzTest/UnitTest1.cs
@@ -1,9 +1,18 @@
 using Bunit;
 using Xunit;
 using Gotorz.Components.Pages;
+using Gotorz.Components.Account.Pages;
 using Microsoft.AspNetCore.Components;
 using Gotorz.Client.Pages;
 using Microsoft.Extensions.DependencyInjection;
+using Bunit.TestDoubles;
+using Gotorz.Components;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gotorz.Data;
 
 namespace TestProject1
 {
@@ -23,5 +32,30 @@ namespace TestProject1
             // Assert
             Assert.Equal("http://localhost/flightsearch", mockNav.Uri);
         }
+
+		[Fact]
+		public void Should_Show_Page_When_Authorized()
+		{
+
+			var roleManagerMock = new Mock<RoleManager<IdentityRole>>(
+				new Mock<IRoleStore<IdentityRole>>().Object,
+				null, null, null, null);
+			var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+			var userManagerMock = new Mock<UserManager<ApplicationUser>>(
+				userStoreMock.Object, null, null, null, null, null, null, null, null);
+
+
+			using var ctx = new TestContext();
+			ctx.Services.AddSingleton(roleManagerMock.Object);
+			ctx.Services.AddSingleton(userManagerMock.Object);
+			var authContext = ctx.AddTestAuthorization();
+			authContext.SetAuthorized("Admin", AuthorizationState.Unauthorized);
+
+			// Act
+			var cut = ctx.RenderComponent<AdminDashboard>();
+
+			// Assert
+			Assert.Contains("Only users with the Admin role can see this page.", cut.Markup);
+		}
 	}
 }
